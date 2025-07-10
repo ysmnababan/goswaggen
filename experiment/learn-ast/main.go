9188765f5eabbed87876b10e24e9229fc980077b
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-07-11 02:37:24 +0700

    refactor: enhance AST inspection to associate functions with comments

--- experiment/learn-ast/main.go
+++ experiment/learn-ast/main.go
@@ -42,35 +42,83 @@ func bar(t string) {
 		}
 	}
 
-	// for _, val := range f.Decls {
-	// fmt.Println(val)
-	// fmt.Println(val.Pos(), val.End())
-	// }
-	// ast.Print(fset, f)
-
-	var functionPos int
+	funPositionMap := make(map[int]*ast.FuncDecl)
+	commentPositionMap := make(map[int]*ast.CommentGroup)
 	ast.Inspect(f, func(n ast.Node) bool {
-		fun, ok := n.(*ast.FuncDecl)
-
-		if ok && fun.Name.Name == "Login" {
-			fmt.Println(fun.Name.Name)
-			fmt.Println(fun.Type.Params.List[0].Names[0].Name)
-			fmt.Println(fun.Type.Params.List[0].Type)
-			fmt.Println(fun.Type.Results.List[0].Names)
-			fmt.Println(fun.Type.Results.List[0].Type)
-			functionPos = fset.Position(fun.Type.Func).Line
-			fmt.Println("function pos:", functionPos)
-		}
 		com, ok := n.(*ast.CommentGroup)
 		if ok {
 			var commentBlockEnd int = fset.Position(com.End()).Line
-			fmt.Println("comment block end:", commentBlockEnd)
-			if functionPos == commentBlockEnd+1 {
-				fmt.Println(com.Text())
+			commentPositionMap[commentBlockEnd] = com
+		}
+
+		fun, ok := n.(*ast.FuncDecl)
+		if ok {
+			if IsEchoHandler(fun) {
+				functionPos := fset.Position(fun.Type.Func).Line
+				funPositionMap[functionPos] = fun
 			}
-			// fmt.Println(fset.Position(com.End()).Line)
-			// ast.Print(fset, com.List)
 		}
 		return true
 	})
+	nodes := AssociateFuncAndComment(funPositionMap, commentPositionMap)
+	for _, node := range nodes {
+		node.PrintFunctionNode()
+	}
+}
+
+func IsEchoHandler(n *ast.FuncDecl) bool {
+	// param is echo context, len is 1
+	param := n.Type.Params.List
+	if len(param) != 1 {
+		return false
+	}
+	field := n.Type.Params.List[0]
+	switch t := field.Type.(type) {
+	case *ast.SelectorExpr:
+		fmt.Printf("Parsed: %s.%s\n", t.X, t.Sel.Name)
+		if ident, ok := t.X.(*ast.Ident); !ok || ident.Name != "echo" {
+			return false
+		}
+		if t.Sel.Name != "Context" {
+			return false
+		}
+	case *ast.StarExpr:
+		if se, ok := t.X.(*ast.SelectorExpr); ok {
+			// X = "echo", Sel = "Context"
+			ident, _ := se.X.(*ast.Ident)
+			fmt.Printf("Parsed: *%s.%s\n", ident.Name, se.Sel.Name)
+			if ident.Name != "echo" || se.Sel.Name != "Context" {
+				return false
+			}
+		}
+	}
+
+	// result param = 1, type is error
+	resParam := n.Type.Results.List
+	if len(resParam) != 1 {
+		return false
+	}
+
+	field = resParam[0]
+	switch t := field.Type.(type) {
+	case *ast.SelectorExpr:
+		if ident, ok := t.X.(*ast.Ident); !ok || ident.Name != "error" {
+			return false
+		}
+	}
+	return true
+}
+
+func AssociateFuncAndComment(fpos map[int]*ast.FuncDecl, cpos map[int]*ast.CommentGroup) []*HandlerNode {
+	nodes := make([]*HandlerNode, 0, len(fpos))
+	for pos, val := range fpos {
+		node := &HandlerNode{
+			fn: val,
+		}
+		if comment, ok := cpos[pos-1]; ok {
+			node.comment = comment
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
