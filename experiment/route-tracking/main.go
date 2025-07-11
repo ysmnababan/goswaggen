900745f9bfc9b625282182ea2c49060d95dc799d
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-07-11 21:54:50 +0700

    feat: refactor main function and add TryLoadAll and TryFindMainFile for package loading

--- experiment/route-tracking/main.go
+++ experiment/route-tracking/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
 	"go/token"
+	"strings"
 
 	"golang.org/x/tools/go/packages"
 )
 
-func main() {
+func TryLoadAll() {
 	cfg := &packages.Config{
 		Mode: packages.NeedName |
-			packages.NeedFiles|
+			packages.NeedFiles |
 			packages.NeedCompiledGoFiles |
 			packages.NeedImports |
 			packages.NeedDeps |
 			packages.NeedTypes |
 			packages.NeedSyntax |
 			packages.NeedTypesInfo,
-		Dir:  "../example/learn-go/internals", // relative to where you run `go run`
+		Dir:  "../example/learn-go/", // relative to where you run `go run`
 		Fset: token.NewFileSet(),
 	}
 	pkgs, err := packages.Load(cfg, "./...")
@@ -26,6 +28,79 @@ func main() {
 	}
 
 	for _, pkg := range pkgs {
-		fmt.Println(pkg.PkgPath)
+		fmt.Println(pkg.GoFiles)
 	}
+}
+
+var fset *token.FileSet
+
+func TryFindMainFile() {
+	fset = token.NewFileSet()
+	cfg := &packages.Config{
+		Mode: packages.NeedName |
+			packages.NeedFiles |
+			packages.NeedCompiledGoFiles |
+			packages.NeedImports |
+			packages.NeedDeps |
+			packages.NeedTypes |
+			packages.NeedSyntax |
+			packages.NeedTypesInfo,
+		Dir:  "../example/learn-go/", // relative to where you run `go run`
+		Fset: fset,
+	}
+	pkgs, err := packages.Load(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	var mainFile *ast.FuncDecl = nil
+	var imports []*ast.ImportSpec = nil
+
+	for _, pkg := range pkgs {
+		// each package contains many file, i.e main.go
+		for i, file := range pkg.Syntax {
+			fmt.Println(pkg.GoFiles[i], "=>>")
+			mainFile = SearchFuncNode(file, "main")
+			if mainFile != nil {
+				imports = file.Imports
+				break
+			}
+		}
+	}
+
+	var echoIdent string
+	echoImport := FindLibrary(imports, "github.com/labstack/echo/v4")
+	if echoImport != nil {
+		if echoImport.Name != nil {
+			echoIdent = echoImport.Name.Name
+		} else {
+			echoIdent = "echo"
+		}
+	}
+	fmt.Println("echo import name", echoIdent)
+}
+
+func FindLibrary(imports []*ast.ImportSpec, targetName string) *ast.ImportSpec {
+	for _, val := range imports {
+		if strings.Contains(val.Path.Value, targetName) {
+			return val
+		}
+	}
+	return nil
+}
+func SearchFuncNode(f *ast.File, targetName string) *ast.FuncDecl {
+	var fnode *ast.FuncDecl = nil
+	ast.Inspect(f, func(n ast.Node) bool {
+		fun, ok := n.(*ast.FuncDecl)
+		if ok && fun.Name.Name == targetName {
+			fnode = fun
+			return false
+		}
+		return true
+	})
+	return fnode
+}
+
+func main() {
+	TryFindMainFile()
 }
