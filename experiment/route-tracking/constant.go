2db46c0bd884afa5dafc766be5b780f263ecd5a9
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-07-21 23:41:37 +0700

    feat: add object type imports and variable types for Echo and EchoGroup

--- experiment/route-tracking/constant.go
+++ experiment/route-tracking/constant.go
@@ -3,3 +3,10 @@ package main
 var IMPORT_PATH_VALUE = map[string]string{
 	"echo": "github.com/labstack/echo/v4",
 }
+
+var OBJECT_TYPE_IMPORT = map[string]string{
+	"ECHO":       "*github.com/labstack/echo/v4.Echo",
+	"ECHO_GROUP": "*github.com/labstack/echo/v4.Group",
+}
+var ECHO_VARIABLE_TYPE = "*github.com/labstack/echo/v4.Echo"
+var ECHO_GROUP_VARIABLE_TYPE = "*github.com/labstack/echo/v4.Group"
