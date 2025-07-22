d963c5773d88ff70c48d3a87a9bb24ce0e614664
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-07-22 23:51:17 +0700

    feat: refactor RegistrationContext to improve function declaration mapping and cache management

--- experiment/recursive_tracking/registration_context.go
+++ experiment/recursive_tracking/registration_context.go
@@ -2,32 +2,57 @@ package main
 
 import (
 	"go/ast"
+	"go/types"
 
 	"golang.org/x/tools/go/packages"
 )
 
 type RegistrationContext struct {
-	Pkgs             []*packages.Package
-	GroupPath        map[string]string
-	// CurrentPackage   *packages.Package
-	CurrentExpr      *ast.CallExpr
-	CurrentFunc      *ast.FuncDecl
-	funcDeclToPkgMap map[*ast.FuncDecl]*packages.Package // for faster retrival of a particular package
+	Pkgs                  []*packages.Package
+	GroupPath             map[string]string
+	funcDeclToPkgMap      map[*ast.FuncDecl]*packages.Package // cache for faster retrival of a particular package
+	typeFuncToFuncDeclMap map[*types.Func]*ast.FuncDecl       // cache for faster retrival of a function declaration
+	CurrentExpr           *ast.CallExpr
+	CurrentFunc           *ast.FuncDecl
 }
 
-func (c *RegistrationContext) BuildFuncDeclToPkgMap() {
+func NewRegistrationContext(pkgs []*packages.Package, funDecl *ast.FuncDecl) *RegistrationContext {
+	ctx := &RegistrationContext{
+		funcDeclToPkgMap:      make(map[*ast.FuncDecl]*packages.Package),
+		typeFuncToFuncDeclMap: make(map[*types.Func]*ast.FuncDecl),
+		GroupPath:             make(map[string]string),
+		Pkgs:                  pkgs,
+		CurrentFunc:           funDecl,
+	}
+	ctx.buildFuncCache()
+	return ctx
+}
+
+func (c *RegistrationContext) buildFuncCache() {
 	declToPkg := make(map[*ast.FuncDecl]*packages.Package)
+	typeFuncToFuncDeclMap := make(map[*types.Func]*ast.FuncDecl)
 	for _, pkg := range c.Pkgs {
 		for _, file := range pkg.Syntax {
 			for _, decl := range file.Decls {
 				if fn, ok := decl.(*ast.FuncDecl); ok {
 					declToPkg[fn] = pkg
+					if obj, ok := pkg.TypesInfo.Defs[fn.Name]; ok {
+						if fnObj, ok := obj.(*types.Func); ok {
+							typeFuncToFuncDeclMap[fnObj] = fn
+						}
+					}
 				}
 			}
 		}
 	}
 	c.funcDeclToPkgMap = declToPkg
+	c.typeFuncToFuncDeclMap = typeFuncToFuncDeclMap
 }
+
 func (c *RegistrationContext) GetCurrentPackage() *packages.Package {
 	return c.funcDeclToPkgMap[c.CurrentFunc]
+}
+
+func (c *RegistrationContext) GetFuncDecl(fnObj *types.Func) *ast.FuncDecl {
+	return c.typeFuncToFuncDeclMap[fnObj]
 }
