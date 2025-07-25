d7f06fbb8f79bc56aa03eaeae2649a5ae3b612c4
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-07-26 01:12:51 +0700

    feat: enhance HandlerRegistration struct with additional fields and methods

--- experiment/parser/handler_registration.go
+++ experiment/parser/handler_registration.go
@@ -5,15 +5,22 @@ import (
 	"go/ast"
 	"go/types"
 	"strings"
+
+	"golang.org/x/tools/go/packages"
 )
 
 type HandlerRegistration struct {
-	Func      *types.Func   // The resolved handler function
+	Func      *types.Func   // The resolved handler function as obj
 	Call      *ast.CallExpr // The actual call expression
-	IsDirect  bool          // True if registered directly on Echo instance
+	IsDirect  bool          // True if registered directly on Framework instance
 	GroupPath string        // Group path segments, e.g., ["/api", "/v1"]
 	BasePath  string        // Combined path, e.g., "/api/v1/resource"
 	FromFunc  *types.Func   // The function where this registration happens
+	Pkg       *packages.Package
+	FuncDecl  *ast.FuncDecl // The implementation of the handler function
+
+	//
+	Request []*RequestData // The requested data can be from body payload, query param or param
 }
 
 func (n *HandlerRegistration) Print() {
@@ -28,4 +35,25 @@ func (n *HandlerRegistration) Print() {
 		fmt.Printf("%s(%v)\n", method.String(), n.Call.Args)
 	}
 	fmt.Println()
+}
+
+func (n *HandlerRegistration) GetFuncName() string {
+	return n.Func.Name()
+}
+
+func (n *HandlerRegistration) GetMethod() string {
+	method := n.Call.Fun.(*ast.SelectorExpr)
+	return method.Sel.Name
+}
+
+func (n *HandlerRegistration) GetFullPath() string {
+	pathArg := n.Call.Args[0].(*ast.BasicLit)
+	return n.BasePath + strings.Trim(pathArg.Value, `"`)
+}
+
+type RequestData struct {
+	Call       *ast.CallExpr // The actual call expression
+	Param      *types.Var    // The param type
+	BindMethod string        // Body, QueryParam, Param
+	BasicLit   string        // for queryparam and param args, e.g. <context>.Param("this")
 }
