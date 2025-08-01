073b5e4448ed781c14971c9651f9d16a40c15bca
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-08-01 16:58:17 +0700

    feat: expand HandlerContext interface with additional type-related methods

--- experiment/parser/context/handler_func_context.go
+++ experiment/parser/context/handler_func_context.go
@@ -1,7 +1,16 @@
 package context
 
-import "golang.org/x/tools/go/packages"
+import (
+	"go/ast"
+	"go/types"
+
+	"golang.org/x/tools/go/packages"
+)
 
 type HandlerContext interface {
 	GetPackage() *packages.Package
+	GetTypesInfo() *types.Info
+	GetTypeNameToGenDeclCache() map[*types.TypeName]*ast.GenDecl
+	GetTypePackageCache() map[*types.Package]*packages.Package
+	GetTypeGlobalVarToValueMap() map[string]string
 }
