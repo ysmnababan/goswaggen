e9284e8809c142d380803631dc6cd3a76db35158
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-08-07 15:43:14 +0700

    feat: add FrameWork field to ReturnResponse struct for framework identification

--- experiment/parser/model/handler_response.go
+++ experiment/parser/model/handler_response.go
@@ -8,5 +8,6 @@ type ReturnResponse struct {
 	SchemaType string // {object}, string, int, etc
 	StatusCode int
 	IsSuccess  bool
-	AcceptType string //json, xml, string
+	AcceptType string // json, xml, string
+	FrameWork  string // echo, gin,
 }
