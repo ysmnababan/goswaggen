28c1f415ccedd45a8d9b830e7b534b1bff30fb8d
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-08-08 18:35:26 +0700

    refactor: delete unnecessary struct

--- experiment/generator/model/comment_block.go
+++ experiment/generator/model/comment_block.go
@@ -7,21 +7,14 @@ type Param struct {
 	IsRequired  bool
 	Description string
 }
-type ResponseBlock struct {
-	StructType string
-	SchemaType string // {object}, string, int, etc
-	StatusCode int
-	IsSuccess  bool
-	AcceptType string // json, xml, string
-}
 
 type CommentBlock struct {
-	Summary        string   // same as function name
-	Description    string   // same as function name with better formating
-	Tags           string   // `___` by default
-	Accept         string   // info from the binding param
-	Produce        []string // info from response
-	Params         []string // info from the binding
-	ResponseBlocks []string // info from the  response
-	Router         string   // info from the registration
+	Summary     string   // same as function name
+	Description string   // same as function name with better formating
+	Tags        string   // `___` by default
+	Accept      string   // info from the binding param
+	Produce     []string // info from response
+	Params      []string // info from the binding
+	Response    []string // info from the  response
+	Router      string   // info from the registration
 }
