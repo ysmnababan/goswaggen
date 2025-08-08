7b694944c2aa887b1bf7394e1617b64d23d4840f
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-08-08 18:34:49 +0700

    refactor: separate constant

---
+++ experiment/generator/constant.go
@@ -0,0 +1,35 @@
+package generator
+
+var DEFAULT_PARAM_DESCRIPTION = "change this description"
+var DEFAULT_QUERY_PARAM_DESCRIPTION = "change this description"
+var DEFAULT_BODY_DESCRIPTION = "change this description"
+var DEFAULT_RESPONSE_SCHEME_TYPE = "object"
+var DEFAULT_FAILURE_RESPONSE_DESCRIPTION = "error"
+var DEFAULT_SUCCESS_RESPONSE_DESCRIPTION = "success"
+var RESPONSE_BLOCK_TEMPLATE = "@%s %d {%s} %s %s"
+var GO_TO_SWAGGO_SCHEME_TYPES_MAP = map[string]string{
+	"bool":      "boolean",
+	"string":    "string",
+	"int":       "integer",
+	"int8":      "integer",
+	"int16":     "integer",
+	"int32":     "integer",
+	"int64":     "integer",
+	"uint":      "integer",
+	"uint8":     "integer",
+	"uint16":    "integer",
+	"uint32":    "integer",
+	"uint64":    "integer",
+	"float32":   "number",
+	"float64":   "number",
+	"[]byte":    "string", // Usually encoded as base64 strings
+	"time.Time": "string", // Formatted datetime string
+	"file":      "file",   // For file uploads/downloads
+	// And for structured types:
+	"struct": "object",
+	"map":    "object",
+	"slice":  "array",
+	"json":   "object",
+	// Add your custom types as needed:
+	// "MyModel": "object",
+}
