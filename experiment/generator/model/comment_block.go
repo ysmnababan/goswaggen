7f12203110237eead25d170ddec33cdd54f7e539
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-08-08 16:34:32 +0700

    fix: change IsRequired type from string to bool in Param struct

--- experiment/generator/model/comment_block.go
+++ experiment/generator/model/comment_block.go
@@ -4,7 +4,7 @@ type Param struct {
 	Name        string
 	BindMethod  string
 	ParamType   string
-	IsRequired  string
+	IsRequired  bool
 	Description string
 }
 type ResponseBlock struct {
@@ -16,12 +16,12 @@ type ResponseBlock struct {
 }
 
 type CommentBlock struct {
-	Summary        string          // same as function name
-	Description    string          // same as function name with better formating
-	Tags           string          // `___` by default
-	Accept         string          // info from the binding param
-	Produce        []string        // info from response
-	Params         []Param         // info from the binding
-	ResponseBlocks []ResponseBlock // info from the  response
-	Router         string          // info from the registration
+	Summary        string   // same as function name
+	Description    string   // same as function name with better formating
+	Tags           string   // `___` by default
+	Accept         string   // info from the binding param
+	Produce        []string // info from response
+	Params         []string // info from the binding
+	ResponseBlocks []string // info from the  response
+	Router         string   // info from the registration
 }
