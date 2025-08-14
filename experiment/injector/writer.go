d6edf2562cda6a9d68abcadb55158087ce1f70ed
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-08-14 21:14:12 +0700

    fix: update parseFunc to return the output file and adjust printSelf function accordingly

--- experiment/injector/writer.go
+++ experiment/injector/writer.go
@@ -4,18 +4,24 @@ import (
 	"bytes"
 	"fmt"
 	"go/ast"
+	"go/format"
 	"go/parser"
 	"go/printer"
 	"go/token"
+	"log"
+	"os"
 	"strings"
 )
 
-func parseFunc(filename, functionname string) (fun *ast.FuncDecl, fset *token.FileSet) {
+func parseFunc(filename, functionname string) (outfile *ast.File, fun *ast.FuncDecl, fset *token.FileSet) {
 	fset = token.NewFileSet()
 	if file, err := parser.ParseFile(fset, filename, nil, 0); err == nil {
+		fmt.Println(file.Comments)
 		for _, d := range file.Decls {
 			if f, ok := d.(*ast.FuncDecl); ok && f.Name.Name == functionname {
+				f.Name.Name = "printSelves"
 				fun = f
+				outfile = file
 				return
 			}
 		}
@@ -23,11 +29,12 @@ func parseFunc(filename, functionname string) (fun *ast.FuncDecl, fset *token.Fi
 	panic("function not found")
 }
 
+// comment before
 func printSelf() {
 	// Parse source file and extract the AST without comments for
 	// this function, with position information referring to the
 	// file set fset.
-	funcAST, fset := parseFunc("writer.go", "printSelf")
+	file, funcAST, fset := parseFunc("writer.go", "printSelf")
 
 	// Print the function body into buffer buf.
 	// The file set is provided to the printer so that it knows
@@ -44,9 +51,17 @@ func printSelf() {
 
 	// Print the cleaned-up body text to stdout.
 	fmt.Println(s)
+
+	srcFile, err := os.Create("output.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = format.Node(srcFile, fset, file)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	printSelf()
-
 }
