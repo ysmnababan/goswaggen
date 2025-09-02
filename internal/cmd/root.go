d35246ba5e4917c8bf5a96cf22b5681d4d67da52
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-09-02 22:56:46 +0700

    refactor: Consolidate version handling by removing version command and integrating version variable in root command

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -7,10 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// overridden at build time
+// go build -ldflags="-X 'github.com/ysmnababan/goswaggen/internal/cmd.version=v0.2.3'" -o goswaggen.exe
+var version = "v1.0.0" 
 var rootCmd = &cobra.Command{
-	Use:   "goswaggen",
-	Short: "Goswaggen is a cli tool for generating handler comment block",
-	Long:  "Goswaggen is a command-line tool that helps you automatically generate Swagger (OpenAPI) comment annotations for your Go HTTP handlers.",
+	Use:     "goswaggen",
+	Short:   "Goswaggen is a cli tool for generating handler comment block",
+	Long:    "Goswaggen is a command-line tool that helps you automatically generate Swagger (OpenAPI) comment annotations for your Go HTTP handlers.",
+	Version: version,
 	Run: func(cmd *cobra.Command, args []string) {
 		_ = cmd.Help()
 	},
