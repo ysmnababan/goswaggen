a71d1155d87fa98bd377536b6f03d0a54a905574
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-09-02 21:51:25 +0700

    feat: Implement InitConfig function to generate config file and add corresponding tests

--- internal/cmd/configinit.go
+++ internal/cmd/configinit.go
@@ -1,12 +1,17 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+var YamlConfigTemplate string = `error_response: "default.Error"
+success_response: "default.Success"`
+var YamlConfigName string = "swaggo_conf.yaml"
 var initCmd = &cobra.Command{
 	Use:     "init",
 	Aliases: []string{"i"},
@@ -14,7 +19,12 @@ var initCmd = &cobra.Command{
 	Long:    "Initialize a config file for customization",
 	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := InitConfig()
+		f, err := os.Create(YamlConfigName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error while generating config file: %v\n", err)
+			os.Exit(1)
+		}
+		err = InitConfig(f)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error while generating config file: %v\n", err)
 			os.Exit(1)
@@ -23,7 +33,13 @@ var initCmd = &cobra.Command{
 	},
 }
 
-func InitConfig() error {
+func InitConfig(out io.Writer) error {
+	w := bufio.NewWriter(out)
+	_, err := w.WriteString(YamlConfigTemplate)
+	if err != nil {
+		return err
+	}
+	w.Flush()
 	return nil
 }
 
