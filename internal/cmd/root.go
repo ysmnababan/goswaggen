889972dda099eefe0d8ef20ead0d07dc1763f721
ysmnababan <170087691+ysmnababan@users.noreply.github.com>
2026-05-08 01:38:15 +0700

    fix: set version at runtime to correctly read build info

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -25,16 +25,17 @@ func getVersion() string {
 }
 
 var rootCmd = &cobra.Command{
-	Use:     "goswaggen",
-	Short:   "Goswaggen is a cli tool for generating handler comment block",
-	Long:    "Goswaggen is a command-line tool that helps you automatically generate Swagger (OpenAPI) comment annotations for your Go HTTP handlers.",
-	Version: getVersion(),
+	Use:   "goswaggen",
+	Short: "Goswaggen is a cli tool for generating handler comment block",
+	Long:  "Goswaggen is a command-line tool that helps you automatically generate Swagger (OpenAPI) comment annotations for your Go HTTP handlers.",
+	// Version: getVersion(),
 	Run: func(cmd *cobra.Command, args []string) {
 		_ = cmd.Help()
 	},
 }
 
 func Execute() {
+	rootCmd.Version = getVersion()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error while executing gocli\n")
 		os.Exit(1)
